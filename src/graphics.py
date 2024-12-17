2bae6467d788c7407b08cdeef5743828bd77d721
Kurlgargyey <nahsirkivar@gmail.com>
2024-12-17 15:22:25 +0100

    rename graphics module to bundle graphics functionality

--- src/graphics.py
+++ src/graphics.py
@@ -22,3 +22,14 @@ class Window:
 
 	def close(self):
 		self.__running = False
+
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+
+class Line:
+	def __init__(self, p1, p2):
+		self.p1 = p1
+		self.p2 = p2
